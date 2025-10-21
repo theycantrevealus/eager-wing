88b8732393d8b5ae88d9d46f7610e28f33e2f8ba
Hendry Tanaka <hendrytanaka10@icloud.com>
2025-10-22 02:12:09 +0700

    chore: character creation init feature

--- src/components/Character.Creation.vue
+++ src/components/Character.Creation.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="scene-container">
     <canvas ref="canvasEl" class="w-full h-full"></canvas>
+    <div id="characterCreation-topPanel"></div>
+    <div id="characterCreation-leftPanel"></div>
+    <div id="characterCreation-rightPanel"></div>
+    <div id="characterCreation-bottomoverlay"></div>
   </div>
 </template>
 <script lang="ts">
-import { __CharacterCreation__ } from "__&GL/render/character.creation"
+import { EagerWing__CharacterCreation } from "__&GL/render/character.creation"
 
 export default {
   name: "CharacterCreation",
   data() {
     return {
-      core: null as __CharacterCreation__ | null,
+      core: null as EagerWing__CharacterCreation | null,
     }
   },
   mounted() {
     const canvas = this.$refs.canvasEl as HTMLCanvasElement
     if (!canvas) return
 
-    this.core = new __CharacterCreation__(canvas)
+    this.core = new EagerWing__CharacterCreation(canvas)
   },
   beforeUnmount() {
     this.core?.destroy()
