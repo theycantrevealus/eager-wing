3848abf020fe6d7a5e687d70a50975abe953e5d7
Hendry Tanaka <hendrytanaka10@icloud.com>
2025-10-22 16:12:27 +0700

    chore: map image assets mapping

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
+      "@assets": path.resolve(__dirname, "src/assets"),
       "__&types": path.resolve(__dirname, "src/types"),
       "__&interfaces": path.resolve(__dirname, "src/interfaces"),
       "__&constants": path.resolve(__dirname, "src/constants"),
