77847298403bc447a427695f80f7154641b41bd5
Hendry Tanaka <hendrytanaka10@icloud.com>
2025-11-15 16:20:25 +0700

    chore: rename module name

--- apps/server/apps/@gateway-core/src/main.ts
+++ apps/server/apps/@gateway-core/src/main.ts
@@ -1,9 +1,68 @@
-import { NestFactory } from "@nestjs/core"
-import type { NestExpressApplication } from "@nestjs/platform-express"
-import { AppModule } from "./app.module"
+/**
+ * @fileoverview Gateway Core
+ * @module bootstrap
+ */
 
+import { NestFactory } from "@nestjs/core"
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from "@nestjs/platform-fastify"
+
+import * as fs from "fs"
+import * as path from "path"
+import * as winston from "winston"
+import { GatewayCoreModule } from "./gateway.core.module"
+
+declare const module: any
+
+/**
+ * Gateway Core Bootstrapper
+ * Start gateway core functionality
+ *
+ */
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  let fastifyAdapter: FastifyAdapter
+  const protocol: boolean = process.env.NODE_SECURE === "true"
+
+  if (protocol) {
+    fastifyAdapter = new FastifyAdapter({
+      logger: false,
+      disableRequestLogging: true,
+      ignoreTrailingSlash: true,
+      ignoreDuplicateSlashes: true,
+      https: {
+        ca: fs.readFileSync(path.resolve(__dirname, "certificates/CA.pem")),
+        pfx: fs.readFileSync(
+          path.resolve(__dirname, "certificates/localhost.pfx"),
+        ),
+        // requestCert: true,
+        rejectUnauthorized: true,
+        key: fs.readFileSync(
+          path.resolve(__dirname, "certificates/localhost.decrypted.key"),
+        ),
+        cert: fs.readFileSync(
+          path.resolve(__dirname, "certificates/localhost.crt"),
+        ),
+        passphrase: process.env.CA_PASS,
+      },
+    })
+  } else {
+    fastifyAdapter = new FastifyAdapter({
+      logger: false,
+      disableRequestLogging: true,
+      ignoreTrailingSlash: true,
+      ignoreDuplicateSlashes: true,
+    })
+  }
+
+  const app = await NestFactory.create<NestFastifyApplication>(
+    GatewayCoreModule,
+    fastifyAdapter,
+    {
+      logger: ["verbose", "error", "warn"],
+    },
+  )
   await app.listen(process.env.PORT || 3000)
   console.log("Gateway Core")
 }
